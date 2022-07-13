997df134d97e80a5fc64b027314e8029f54bbf2b
PipeRojas <andresf_135@hotmail.com>
2022-07-13 13:15:14 -0500

    Initial commit & Solution

---
+++ src/main/java/edu/codelab/herokuCodelab/HerokuCodelabApplication.java
@@ -0,0 +1,13 @@
+package edu.codelab.herokuCodelab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuCodelabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuCodelabApplication.class, args);
+	}
+
+}
